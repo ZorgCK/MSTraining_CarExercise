dae934b245af09f37295e44b5700b5e34a8b1003
Kümmel Christian <c.kuemmel@microstream.one>
2022-10-11 09:30:55 +0200

    Change MS instantiation to builder pattern

--- src/main/java/one/microstream/storage/DB.java
+++ src/main/java/one/microstream/storage/DB.java
@@ -1,10 +1,5 @@
 package one.microstream.storage;
 
-import java.net.URL;
-import java.util.Optional;
-
-import io.micronaut.core.io.ResourceResolver;
-import io.micronaut.core.io.scan.ClassPathResourceLoader;
 import one.microstream.domain.Company;
 import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
 import one.microstream.storage.embedded.types.EmbeddedStorageManager;
@@ -16,11 +11,10 @@ public class DB
 	public final static Company				root	= new Company();
 	
 	static
-	{
-		ClassPathResourceLoader loader = new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
-		Optional<URL> resource = loader.getResource("microstream.xml");
-		
-		storageManager = EmbeddedStorageConfiguration.load(
-			resource.get().getPath()).createEmbeddedStorageFoundation().createEmbeddedStorageManager(root).start();
+	{		
+		storageManager = EmbeddedStorageConfiguration.Builder()
+			.createEmbeddedStorageFoundation()
+			.createEmbeddedStorageManager(root)
+			.start();
 	}
 }
