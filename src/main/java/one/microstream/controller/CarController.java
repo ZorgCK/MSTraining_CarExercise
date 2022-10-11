663ae161006d5dca2612a11f8d5432df038b8b25
Kümmel Christian <c.kuemmel@microstream.one>
2022-10-11 09:35:53 +0200

    Clear controller and db code

--- src/main/java/one/microstream/controller/CarController.java
+++ src/main/java/one/microstream/controller/CarController.java
@@ -1,8 +1,6 @@
 package one.microstream.controller;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.HttpResponse;
@@ -11,9 +9,7 @@ import io.micronaut.http.annotation.Delete;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.QueryValue;
-import one.microstream.domain.Brand;
 import one.microstream.domain.Car;
-import one.microstream.storage.DB;
 
 
 @Controller("/cars")
@@ -22,52 +18,30 @@ public class CarController
 	@Get
 	public List<Car> listCars()
 	{
-		return DB.root.getCars();
+		return null;
 	}
 	
 	@Get(value = "/bymodel")
 	public List<Car> getCarsByModel(@Nullable @QueryValue String model)
 	{
-		return DB.root.getCars().stream().filter(car -> car.getModel().equalsIgnoreCase(model)).collect(
-			Collectors.toList());
+		return null;
 	}
 	
 	@Get(value = "/bybrand")
 	public List<Car> getCarsByBrand(@Nullable @QueryValue String brand)
 	{
-		return DB.root.getCars().stream().filter(car -> car.getBrand().getName().equalsIgnoreCase(brand)).collect(
-			Collectors.toList());
+		return null;
 	}
 	
 	@Delete
 	public HttpResponse<String> clearCars()
 	{
-		DB.root.getCars().clear();
-		DB.storageManager.store(DB.root.getCars());
-		
-		return HttpResponse.ok("Cars cleared successfully");
+		return null;
 	}
 	
 	@Post(value = "/insert")
 	public Car insertCar(Car dto)
 	{
-		Optional<Brand> optionalBrand = DB.root.getBrands().stream().filter(
-			brand -> brand.getName().equalsIgnoreCase(dto.getBrand().getName())).findFirst();
-		
-		if(optionalBrand.isPresent())
-		{
-			Brand brand = optionalBrand.get();
-			
-			Car car = new Car();
-			car.setBrand(brand);
-			car.setModel(dto.getModel());
-			
-			DB.root.getCars().add(car);
-			DB.storageManager.store(DB.root.getCars());
-			
-			return car;
-		}
-		
 		return null;
 	}
 }
--- src/main/java/one/microstream/storage/DB.java
+++ src/main/java/one/microstream/storage/DB.java
@@ -1,20 +1,10 @@
 package one.microstream.storage;
 
-import one.microstream.domain.Company;
-import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
-import one.microstream.storage.embedded.types.EmbeddedStorageManager;
-
-
 public class DB
 {
-	public static EmbeddedStorageManager	storageManager;
-	public final static Company				root	= new Company();
 	
 	static
 	{		
-		storageManager = EmbeddedStorageConfiguration.Builder()
-			.createEmbeddedStorageFoundation()
-			.createEmbeddedStorageManager(root)
-			.start();
+
 	}
 }
